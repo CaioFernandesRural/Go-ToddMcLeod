cb51697525afd2b00b251f76dc0edb3874195348
caio <caioorebeo@gmail.com>
2022-09-30 18:38:02 -0300

    sesssão 7 completa

---
+++ sessão7/1.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := 412
+	fmt.Printf("%v\t\t%b\t\t%#x\n", x, x, x)
+}
---
+++ sessão7/4.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := `bom dia o caralho aqui e a "torcida" jovem do flamengo`
+
+	fmt.Println(a)
+}
